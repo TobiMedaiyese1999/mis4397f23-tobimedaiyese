989550f9df036a967c7a9e84d7caf8fcfde609e2
Ubuntu <ubuntu@ip-172-31-36-247.us-east-2.compute.internal>
2023-09-19 19:19:08 +0000

    adding pop quiz 2 to repo.

---
+++ pop-quiz-2/main.rb
@@ -0,0 +1,56 @@
+#5 emails, sort the emails by alphabetical order
+
+emails = [
+  "samjackson@gmail.com",
+  "jordaned@gmail.com",
+  "kevinm@gmail.com",
+  "simon@gmail.com",
+  "cory@gmail.com"
+]
+
+sorted_email_addresses = 
+emails.sort.reverse
+
+sorted_email_addresses.each do 
+  |email| 
+    puts email
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# how do i clone my git repository on repl it
+
+# how do i print my current working directory, and create 3 files using BASH...
---
+++ pop-quiz-2/printcurrentdirectory.sh
@@ -0,0 +1,5 @@
+pwd
+touch file1 file2 file3
+pwd & touch file1 file2 file3
+
+git clone https://github.com/TobiMedaiyese1999/mis4397f23-tobimedaiyese.gitto
